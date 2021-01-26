970fb6f1e1c487598fada01dfbbe1650103bd738
dkamalov <dkamalov8@gmail.com>
2021-01-26 15:56:13 -0500

    HomePage
    Added columns and another image

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
-
+import './Home.css';
 
 const Home = () => {
 
     return <div>
         <center> <img src = " https://api.time.com/wp-content/uploads/2019/07/plastic-garbage.jpg" width="900" height="500"></img></center>
+    
+    
+    <br></br>
+    <hr></hr>
+    <br></br>
+    <center> <img src = "https://cdn.akamai.steamstatic.com/steam/apps/382310/capsule_616x353.jpg?t=1611504679 "></img></center>
+    <br></br>
+    <hr></hr>
+    <br></br>
+    <center>
+        <div class="row">
+            <div class="column">
+                <p> Column, should add description and link to search </p>
+                </div>
+            <div class="column">
+                <p> Column, should add description and link to search </p>
+                </div>
+            <div class="column">
+                <p> Column, should add description and link to search </p>
+                </div>
+        </div>
+    </center>
+    <hr></hr>
+    <br></br>
+    <br></br>
+    <h1> FAQ: </h1>
     </div>
+
+    
 }
 
 export default Home;
