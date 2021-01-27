89ebbf2becacbdcab2cc251cc522fe64b4af0923
dkamalov <dkamalov8@gmail.com>
2021-01-27 13:59:01 -0500

    routes for newitem/newusers

--- src/components/AllPlayers/routes/RoutesContainer.jsx
+++ src/components/AllPlayers/routes/RoutesContainer.jsx
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { Route, Switch } from "react-router-dom";
 import Home from "../../Pages/Home/Home.jsx";
 import ZipSearch from "../../ZipSearch/ZipSearch.jsx";
+import NewItem from "../../databsecomponents/NewUser.js";
+import NewUser from "../../databsecomponents/NewItem.js";
+
 
 class RoutesContainer extends Component {
   render() {
@@ -10,6 +13,8 @@ class RoutesContainer extends Component {
         <Route exact path= "/">
           <Home />
           <ZipSearch />
+          <NewItem />
+          <NewUser />
         </Route>
       </Switch>
     );
